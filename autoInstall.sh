23635840d21e42e2335bf3129db3a4aac382fbab
Vic <vics@comfull.net>
2022-11-15 11:52:45 +0800

    remove: docker review version

--- autoInstall.sh
+++ autoInstall.sh
@@ -13,9 +13,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-docker -v
 sudo service docker start
-sudo service docker status
 
 # openssh
 sudo apt-get install openssh-server
