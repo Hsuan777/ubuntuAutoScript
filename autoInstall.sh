3215a89e6bad3418ed6a5e78a3f7838027210eed
Vic <33892461+Hsuan777@users.noreply.github.com>
2022-12-03 21:36:12 +0800

    add: nvm

--- autoInstall.sh
+++ autoInstall.sh
@@ -23,3 +23,9 @@ sudo apt-get install openssh-server
 
 # vim 
 sudo apt-get install vim
+
+# nvm
+sudo apt install curl 
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.profile 
+nvm install node 
